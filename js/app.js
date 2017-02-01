3561ed1b5af1921a3bfc2480587bc4d0412a821c
Sébastien JEAN <sebastien@medialta.com>
2017-02-01 21:22:13 +0100

    [app] Core structure

---
+++ js/app.js
@@ -0,0 +1,99 @@
+/**
+ * App - Provide useful methods across components
+ * @type {Object}
+ */
+var app = {
+    vue: null,
+
+    init: function()
+    {
+        app.vue = new Vue({
+            el: '#app'
+        })
+    },
+
+    /**
+     * Perform a GET request
+     * @param  {string} url URL of the resource to get
+     * @param  {function|null} successCallback Success callback
+     * @param  {function|null} errorCallback Error callback
+     */
+    get: function(url, successCallback, errorCallback)
+    {
+        var wrappedErrorCallback = function()
+        {
+            if (app.isCallback(errorCallback))
+            {
+                errorCallback(data, xhr)
+            }
+        }
+
+        var xhr = new XMLHttpRequest()
+        xhr.open('GET', url, true)
+
+        xhr.onload = function()
+        {
+            if (xhr.status >= 200 && xhr.status < 400)
+            {
+                var data
+
+                try
+                {
+                    data = JSON.parse(xhr.responseText)
+                }
+                catch (e)
+                {
+                    data = xhr.responseText
+                }
+
+                if (app.isCallback(successCallback))
+                {
+                    successCallback(data, xhr)
+                }
+            }
+            else
+            {
+                wrappedErrorCallback
+            }
+        }
+
+        xhr.onerror = wrappedErrorCallback
+
+        xhr.send()
+    },
+
+    /**
+     * Get a template from a name
+     * @param  {string} name Template name
+     */
+    resolveTemplate: function(name, component)
+    {
+        var path = '/components/' + name + '/' + name + '.html'
+
+        if (!component)
+        {
+            component = {}
+        }
+
+        return function(resolve, reject)
+        {
+            app.get(path, function(template)
+            {
+                component.template = template
+                resolve(component)
+            })
+        }
+    },
+
+    /**
+     * Test if a variable is a valid callback (a function)
+     * @param  {*} callback Anything
+     * @return {Boolean} true if callback is a function
+     */
+    isCallback: function(callback)
+    {
+        return typeof callback === 'function'
+    }
+}
+
+document.addEventListener('DOMContentLoaded', app.init)
