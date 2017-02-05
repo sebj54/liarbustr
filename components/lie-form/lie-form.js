a5c2092e006b218659e6469adafe63bef884e1e5
Sébastien JEAN <sebastien@medialta.com>
2017-02-05 10:43:47 +0100

    [lie-form] Remove useless lie-form-source subcomponent

--- components/lie-form/lie-form.js
+++ components/lie-form/lie-form.js
@@ -33,31 +33,3 @@ Vue.component('lie-form', app.resolveTemplate('lie-form', {
         this.addStatement()
     }
 }))
-
-Vue.component('lie-form-source', app.resolveTemplate('lie-form/lie-form-source', {
-    props: [
-        'index',
-        'placeholder',
-        'type',
-        'value'
-    ],
-    data: function()
-    {
-        return {
-            source: null
-        }
-    },
-    methods: {
-        fieldId: fieldId
-    },
-    watch: {
-        source: function()
-        {
-            this.$emit('input', this.source);
-        }
-    },
-    created: function()
-    {
-        this.source = this.value
-    }
-}))
