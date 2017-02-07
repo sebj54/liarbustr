155919ab8999600c1aa00e354aa71eb23ce193f1
Felix Charlier <felixcharlier@gmail.com>
2017-02-07 23:36:05 +0100

    [header] Header now shows logged user

---
+++ components/app-header/app-header.js
@@ -0,0 +1,17 @@
+Vue.component('app-header', app.resolveTemplate('app-header', {
+
+    data: function()
+    {
+        return {
+            user: user,
+        }
+    },
+
+    computed: {
+
+    },
+
+    methods: {
+
+    }
+}))
