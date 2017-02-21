b8cbaa283479d2b5b92b9bd7e2658e084e4b35b2
Sébastien JEAN <sebastien@medialta.com>
2017-02-21 18:20:35 +0100

    [dev] Add browsersync - HTTP server + live reload

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp')
 var rename = require('gulp-rename')
 var svgmin = require('gulp-svgmin')
 var svgSymbols = require('gulp-svg-symbols')
+var browserSync = require('browser-sync').create()
 
 var svgSymbolsConfig = {
     svgClassname: 'svg-master',
@@ -17,4 +18,24 @@ gulp.task('compile-svg', function()
     .pipe(svgSymbols(svgSymbolsConfig))
     .pipe(rename('svg-master.svg'))
     .pipe(gulp.dest('svg'))
+})
+
+gulp.task('server', function()
+{
+    browserSync.init({
+        host: 'liarbustr.com',
+        open: 'external',
+        notify: false,
+        server: './'
+    })
+
+    gulp.watch([
+        'components/**',
+        'css/**',
+        'js/**',
+        'locales/**',
+        'svg/**',
+        'views/**',
+        'index.html'
+    ]).on('change', browserSync.reload)
 })
