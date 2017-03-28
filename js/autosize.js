1c1340f4236b6fcd87cee22ca5cfa480f8d97732
Sébastien JEAN <sebastien@medialta.com>
2017-03-28 12:16:17 +0200

    [app] Autosize directive compatible with text inputs

--- js/autosize.js
+++ js/autosize.js
@@ -1,4 +1,4 @@
-/* global Vue autosize */
+/* global Vue autosize window document */
 
 /**
  * Autosize Vue directive
@@ -8,6 +8,34 @@
 Vue.directive('autosize', {
     inserted: function(el)
     {
-        autosize(el)
+        if (el.tagName === 'TEXTAREA')
+        {
+            autosize(el)
+        }
+        else
+        {
+            const fakeInput = document.createElement('span')
+            const updateFakeInput = function()
+            {
+                fakeInput.innerHTML = el.value || el.getAttribute('placeholder')
+                el.style.width = fakeInput.offsetWidth + 'px'
+            }
+
+            fakeInput.className = el.className + ' -is-fake-input'
+            updateFakeInput()
+
+            el.addEventListener('input', updateFakeInput)
+
+            el.parentNode.appendChild(fakeInput)
+        }
+    },
+    update: function(el)
+    {
+        if (el.tagName !== 'TEXTAREA')
+        {
+            const event = document.createEvent('HTMLEvents')
+            event.initEvent('input', true, false)
+            el.dispatchEvent(event)
+        }
     },
 })
