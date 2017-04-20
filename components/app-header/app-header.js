4d485aeb59fb8a40f91177e3dd9e7b158526e346
Sébastien JEAN <sebastien@medialta.com>
2017-04-20 17:58:47 +0200

    [header] Add responsive behavior

--- components/app-header/app-header.js
+++ components/app-header/app-header.js
@@ -4,16 +4,46 @@ Vue.component('app-header', app.resolveTemplate('app-header', {
     data: function()
     {
         return {
+            /**
+             * True if header's content is toggled
+             * @type {Boolean}
+             */
+            isContentToggled: false,
+            /**
+             * User
+             * @type {object}
+             */
             user: user,
         }
     },
     computed: {
+        /**
+         * User is not actually logged in if true
+         * @return {Boolean}
+         */
         isAnonymous: function()
         {
             return user.isAnonymous
         },
     },
     methods: {
+        /**
+         * Hide header's content
+         */
+        hideContent: function()
+        {
+            this.isContentToggled = false
+        },
+        /**
+         * Log out user
+         */
         logout: user.logout,
+        /**
+         * Toogle header's content
+         */
+        toggleContent: function()
+        {
+            this.isContentToggled = !this.isContentToggled
+        },
     },
 }))
