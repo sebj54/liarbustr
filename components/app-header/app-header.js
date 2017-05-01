2b064a9b7f9cdbf355402f9cd54819b381f997cc
Sébastien JEAN <sebastien@medialta.com>
2017-05-01 18:23:52 +0200

    [app-header] Display not moderated count in a badge

--- components/app-header/app-header.js
+++ components/app-header/app-header.js
@@ -1,4 +1,4 @@
-/* global Vue app user */
+/* global Vue app user _ */
 
 Vue.component('app-header', app.resolveTemplate('app-header', {
     data: function()
@@ -16,6 +16,22 @@ Vue.component('app-header', app.resolveTemplate('app-header', {
             user: user,
         }
     },
+    firebase: function()
+    {
+        let firebaseDatas = null
+
+        if (this.user.isAdmin)
+        {
+            firebaseDatas = {
+                notModerated: {
+                    source: app.db.ref('/collections/recent-unmoderated/lies'),
+                    asObject: true,
+                },
+            }
+        }
+
+        return firebaseDatas
+    },
     computed: {
         /**
          * User is not actually logged in if true
@@ -24,6 +40,27 @@ Vue.component('app-header', app.resolveTemplate('app-header', {
         isAnonymous: function()
         {
             return user.isAnonymous
+        },
+        /**
+         * Count of not moderated lies
+         * @return {integer} Lies count
+         */
+        notModeratedCount: function()
+        {
+            let notModeratedCount = 0
+
+            if (_.hasProp(this, 'notModerated'))
+            {
+                Object.keys(this.notModerated).forEach(function(lieKey)
+                {
+                    if (lieKey !== '.value' && lieKey !== '.key')
+                    {
+                        notModeratedCount += 1
+                    }
+                })
+            }
+
+            return notModeratedCount
         },
     },
     methods: {
